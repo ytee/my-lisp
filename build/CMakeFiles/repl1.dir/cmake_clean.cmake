b94a6a542cbd282c8b0e5a84c1638b687daae1fd
ytee <vaidyanathan.ag@gmail.com>
2018-01-05 11:21:08 +0530

    Initial version

---
+++ build/CMakeFiles/repl1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/repl1.dir/src/repl_basic.c.o"
+  "../bin/repl1.pdb"
+  "../bin/repl1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/repl1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
